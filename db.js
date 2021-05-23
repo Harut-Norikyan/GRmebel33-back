07236d88fc98ef4ff35d07b48f6adbee54f5eff6
Harut <harutnoro@mail.ru>
2021-05-23 13:39:41 +0400

    init

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const connectDb = async () => {
     try {
-        var url = 'mongodb://localhost:27017/GRmebel33';
+        // var url = 'mongodb://localhost:27017/GRmebel33';
         // var url = 'mongodb://5.63.152.244:27017/GRmebel33';
-        // var url = 'mongodb+srv://GRmebel:GagikRoman889@cluster0.t8qsa.mongodb.net/GRmebel33';
+        var url = 'mongodb+srv://GRmebel:GagikRoman889@cluster0.t8qsa.mongodb.net/GRmebel33';
         await mongoose.connect(url, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
